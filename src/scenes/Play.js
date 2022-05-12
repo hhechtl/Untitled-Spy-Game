6ff817ce05db19a485a1ac98dbc6436284c579fe
Santiago Ponce <saponce@ucsc.edu>
2022-05-11 22:47:36 -0700

    Fixing my dumb mistake

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,10 +4,27 @@ class Play extends Phaser.Scene {
     }
 
     preload(){
+        // load tileset and tilemap
+        /* NOTE: We may want to move this to a master 'load' scene, so we don't have to load the tileset
+        every time we want to create a level. - Santiago */
+        this.load.path = 'assets/';
+        this.load.image('tilesCityPH', 'PH_city_tiles.png');
+        this.load.tilemapTiledJSON('lvlDigitalProto', 'levels/level_digital_prototype.json');
+        console.log("Finished loading");
     }
 
-
     create() {
+        //Adding tilemap
+        const mapProto = this.make.tilemap({key: 'lvlDigitalProto'});
+        const tilesCity = mapProto.addTilesetImage('lvlDigitalProto', 'tilesCityPH', 16, 16);
+
+        //Creates layers matching the layers we made in Tiled software
+        let wallsLayer = mapProto.createLayer('Solid', tilesCity);
+        
+        //Makes all tiles that have property "collides" have collision
+        wallsLayer.setCollisionByProperty( {collides: true} );
+
+
         this.add.text(game.config.width/2, game.config.height/2, 'PLAY' ).setOrigin(0.5);
         keyJump = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
         
@@ -21,8 +38,6 @@ class Play extends Phaser.Scene {
         //temp floor
         this.floor = new Floor(this,200,game.config.height);
         this.physics.add.collider(this.plrSpy,this.floor);
-
-        
     }
 
     update(time, delta ) {
