6415acfa05d133c47333ba4a95c57bb2bd1edd83
Santiago Ponce <saponce@ucsc.edu>
2022-05-12 14:18:23 -0700

    Finally got the damn thing to work

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,7 +8,7 @@ class Play extends Phaser.Scene {
         /* NOTE: We may want to move this to a master 'load' scene, so we don't have to load the tileset
         every time we want to create a level. - Santiago */
         this.load.path = 'assets/';
-        this.load.image('tilesCityPH', 'PH_city_tiles.png');
+        this.load.image('tilesCityPH', 'PH_city_tiles_small.png');
         this.load.tilemapTiledJSON('lvlDigitalProto', 'levels/level_digital_prototype.json');
         console.log("Finished loading");
     }
@@ -16,13 +16,17 @@ class Play extends Phaser.Scene {
     create() {
         //Adding tilemap
         const mapProto = this.make.tilemap({key: 'lvlDigitalProto'});
-        const tilesCity = mapProto.addTilesetImage('lvlDigitalProto', 'tilesCityPH', 16, 16);
+        const tilesCity = mapProto.addTilesetImage('PH_city_tiles', 'tilesCityPH');
+        // 1st arg, the tileset name, needs to match the tileset name in the Tiled file (check the program)
 
         //Creates layers matching the layers we made in Tiled software
-        let wallsLayer = mapProto.createLayer('Solid', tilesCity);
+        let solidLayer = mapProto.createLayer('Solid', tilesCity, 0, 0);
+        let platformLayer = mapProto.createLayer('Platform', tilesCity, 0, 0);
+        //Caredful that all of the keys and stuff match what was defined in the Tiled file.
         
         //Makes all tiles that have property "collides" have collision
-        wallsLayer.setCollisionByProperty( {collides: true} );
+        solidLayer.setCollisionByProperty( {collides: true} );
+        platformLayer.setCollisionByProperty( {collides: true} );
 
 
         this.add.text(game.config.width/2, game.config.height/2, 'PLAY' ).setOrigin(0.5);
@@ -38,6 +42,8 @@ class Play extends Phaser.Scene {
         //temp floor
         this.floor = new Floor(this,200,game.config.height);
         this.physics.add.collider(this.plrSpy,this.floor);
+        this.physics.add.collider(this.plrSpy, solidLayer);
+        this.physics.add.collider(this.plrSpy, platformLayer);
     }
 
     update(time, delta ) {
