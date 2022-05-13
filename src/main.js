77ef6a728f10525fb81d7d7d8a54c6909eac5143
Santiago Ponce <saponce@ucsc.edu>
2022-05-13 14:16:13 -0700

    Adjusted camera to show full level

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 let config = {
       type: Phaser.AUTO,
-      width: 400, // 16 x 9 aspect ratio. Can be scaled up by 2 for fullscreen or divided by 3 to get a good pixel art size
-      height: 225,
+      width: 544, // 16 x 9 aspect ratio. Can be scaled up by 2 for fullscreen or divided by 3 to get a good pixel art size
+      height: 480,
       scale: {
         autoCenter: Phaser.Scale.CENTER_BOTH,
-        zoom: 4 //4
+        zoom: 2 //4
       },
       physics: {
           default: 'arcade',
