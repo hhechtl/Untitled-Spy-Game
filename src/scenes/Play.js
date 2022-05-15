e179525f139f53c2649b60172f43c23649735a63
Santiago Ponce <saponce@ucsc.edu>
2022-05-14 17:52:58 -0700

    Got points to be filled

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -14,16 +14,9 @@ class Play extends Phaser.Scene {
         this.load.image('objButton', 'PH_obj_button.png');
     }
 
+    
     create() {
-        //defining keys
-        keyJump = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
-        keyLeft = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-        keyRight = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-        keyJump = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
-        keyDisguise = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.X);
-        keyDown = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
-        keyInteract = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-
+        this.defineKeys()
 
         //Adding tilemap
         const mapProto = this.make.tilemap({key: 'lvlDigitalProto'});
@@ -48,50 +41,35 @@ class Play extends Phaser.Scene {
         })
 
 
+        //Create player + objects
         this.add.text(game.config.width/2, game.config.height/2, 'PLAY' ).setOrigin(0.5);
-        //create player 
         this.plrSpy = new PlayerSpy(this, 100, 50);
+        this.createButtons();
+
+        let rect = this.add.rectangle(50, 50, 300, 300).setStrokeStyle(1, 0xff0000);
 
         //creating detectors for level
         this.degree = 0;
-        this.raycaster;
-        this.ray;
-        this.createDetectors();
-        this.createSpotlights(solidLayer, platformLayer);
-        //Create buttons
-        this.groupButtonObjs = this.add.group([new ObjInteract(this, 272, 32, 'objButton'),
-         new ObjInteract(this, 16, 208, 'objButton'), new ObjInteract(this, 528, 208, 'objButton'),
-         new ObjInteract(this, 272, 432, 'objButton')]);
-        this.groupButtonObjs.runChildUpdate = true;
-        //Create objective tracker
-        this.buttonTracker = new Checklist(this, "buttonTracker", this.groupButtonObjs.countActive());
-
-        //Add event for each button when they are pressed, listening for the signal 'objactivated'
-        let buttons = this.groupButtonObjs.getChildren(); //More like a dict than an array...
-        for(let i = 0; i < buttons.length; i++){
-            let button = buttons[i];
-            button.on('objactivated', () => {
-                this.buttonTracker.addObjective();
-            });
-        }
-        /*With ability to establish events and listeners, we could theoretically add a locked door 
-        (which I'll add later) - Santiago*/
-
+        this.raycaster = this.raycasterPlugin.createRaycaster({debug: true});
+        this.graphics;
+        this.intersections;
+        this.createSpotlights([solidLayer, rect]);
+        
 
         //moving text 
         this.dressedText = this.add.text(game.config.width/2 + 600, game.config.height/2, 'Getting dressed...',{fontSize: '9px'} ).setOrigin(0.5);
-
         this.gameOver = false;
-
 
 
         //colliders
         this.physics.add.collider(this.plrSpy, solidLayer);
         this.platformCollision = this.physics.add.collider(this.plrSpy, platformLayer);
     
+        //Rotates the cone and re-fills the intersections list
         this.rotate = this.time.addEvent({ delay: 1000, callback: () =>{
             this.ray.setAngleDeg(this.degree++);
-            this.intersections =this.ray.castCone();
+            this.intersections = this.ray.castCone();
+            this.drawLOS();
          }, loop: true });
     }
 
@@ -109,39 +87,76 @@ class Play extends Phaser.Scene {
         }
     }
 
-    createDetectors(){
-        this.raycaster = this.raycasterPlugin.createRaycaster();
-        this.raycaster = this.raycasterPlugin.createRaycaster({
-            debug: true
-          });
-    }
-    createSpotlights(){
+
+    //We will have multiple rays per scene, so we may want to set this up so that it loops over all rays or something
+    createSpotlights(mappedObjects){
         //https://github.com/wiserim/phaser-raycaster
         this.ray = this.raycaster.createRay();
+        this.ray.setOrigin(275, 220);
         this.ray.setAngleDeg(this.degree);
         this.ray.setConeDeg(45);
-        //set ray position
-        this.ray.setOrigin(275, 220);
+        
         //enable auto slicing field of view
-        this.ray.autoSlice = true;
-        //enable arcade physics body
+        this.ray.autoSlice = true; 
         this.ray.enablePhysics();
-
+        //Maps objects to the ray so it can collide with them
+        this.raycaster.mapGameObjects(mappedObjects); 
         
         //set collision (field of view) range
         this.ray.setCollisionRange(200);
 
         //cast ray
-        this.intersections =this.ray.castCone();
+        this.intersections = this.ray.castCone();
         
+
+        //Draw ray LoS
+        this.graphics = this.add.graphics({ lineStyle: { width: 1, color: 0x00ff00}, fillStyle: { color: 0xffffff, alpha: 0.3 } });
+        this.drawLOS();
+
+
         //if player is caught in light 
         this.physics.add.overlap(this.ray, this.plrSpy, function(rayFoVCircle, target){
             console.log("detected");
             //target.detectedFunc();
         }, this.ray.processOverlap.bind(this.ray));
-    
-    
     }
 
+    drawLOS(){
+        this.intersections.push(this.ray.origin);
+        //console.log(this.intersections);
+        this.graphics.clear();
+        this.graphics.fillStyle(0xffffff, 0.3);
+        this.graphics.fillPoints(this.intersections);
+    };
 
+    defineKeys(){
+        keyJump = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
+        keyLeft = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRight = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        keyJump = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
+        keyDisguise = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.X);
+        keyDown = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+        keyInteract = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+    }
+
+    createButtons(){
+        //Create buttons
+        this.groupButtonObjs = this.add.group([new ObjInteract(this, 272, 32, 'objButton'),
+        new ObjInteract(this, 16, 208, 'objButton'), new ObjInteract(this, 528, 208, 'objButton'),
+        new ObjInteract(this, 272, 432, 'objButton')]);
+        this.groupButtonObjs.runChildUpdate = true;
+        //Create objective tracker
+        this.buttonTracker = new Checklist(this, "buttonTracker", this.groupButtonObjs.countActive());
+
+        //Add event for each button when they are pressed, listening for the signal 'objactivated'
+        let buttons = this.groupButtonObjs.getChildren(); //More like a dict than an array...
+        for(let i = 0; i < buttons.length; i++){
+        let button = buttons[i];
+        button.on('objactivated', () => {
+            this.buttonTracker.addObjective();
+        });
+        }
+        /*With ability to establish events and listeners, we could theoretically add a locked door 
+        (which I'll add later) - Santiago*/
+    }
 }
